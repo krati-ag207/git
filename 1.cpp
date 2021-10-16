9d506e1538e7f29392f69c7a5b25fee3c437935b
krati-ag207 <agrawalkrati49@gmail.com>
2021-10-16 12:30:02 +0530

    Initial commit with my 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    a =12;
+    cout<<"sice of int "<<sizeof(a)<<endl;
+
+
+    return 0;
+}
---
+++ oddeven.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a nmbr";
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"even"<<endl;
+    }
+    else{
+        cout<<"odd"<<endl;
+    }
+}
